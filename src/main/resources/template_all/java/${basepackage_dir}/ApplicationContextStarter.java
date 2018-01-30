7d38e2abdb1854f3168f4c587362bbc463aca899
jzteam <zhujizhi1012@163.com>
2018-01-30 14:36:12 +0800

    sql table add ``

---
+++ src/main/resources/template_all/java/${basepackage_dir}/ApplicationContextStarter.java
@@ -0,0 +1,17 @@
+<#include "/macro.include">
+<#assign className = table.className>
+<#assign classNameLower = className?uncap_first>
+
+package ${basepackage};
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class ApplicationContextStarter {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ApplicationContextStarter.class, args);
+    }
+}
